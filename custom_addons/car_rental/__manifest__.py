7536a0f9f0f6bbb70d8f51c3c5cb283890ba43a6
Tersom <ibnua0291@gmail.com>
2026-09-20 11:57:13 +0700

    [ADD] car_rental: add rental receipt pdf report
    - Add QWeb PDF receipt report action and A5 paperformat
    - Add receipt template with customer and vehicle details
    - Add terms and conditions inherit template

--- custom_addons/car_rental/__manifest__.py
+++ custom_addons/car_rental/__manifest__.py
@@ -14,6 +14,10 @@
         'views/car_rental_vehicle_views.xml',
         'views/car_rental_order_views.xml',
         'views/res_partner_views.xml',
+        'report/report_paperformat.xml',
+        'report/car_rental_report_templates.xml',
+        'report/car_rental_report.xml',
+        'report/car_rental_report_terms.xml',
         'views/car_rental_menus.xml',
     ],
     'demo': [
