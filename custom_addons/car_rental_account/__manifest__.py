a18a15c1a1c7c1b0ff5e19f13df9fdb40469b845
Tersom <ibnua0291@gmail.com>
2026-09-24 16:09:47 +0700

    [ADD] car_rental_account: add invoicing bridge module
    Create customer invoices from rental orders, link them on
    account.move, and show a Paid ribbon from invoice payment_state.

---
+++ custom_addons/car_rental_account/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Car Rental - Accounting Bridge',
+    'version': '1.0',
+    'category': 'Services',
+    'summary': 'Integrasi Car Rental dengan Invoicing',
+    'author': 'Tersom',
+    'depends': ['car_rental','account'],
+    'data': [
+        'views/car_rental_order_views.xml',
+        'views/account_move.xml'
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ custom_addons/car_rental_account/models/account_move.py
@@ -0,0 +1,15 @@
+from odoo import models,fields,api
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    car_rental_order_id = fields.Many2one('car.rental.order', string='Order Sewa')
+
+    def action_view_order(self):
+        self.ensure_one()
+        return{
+            'type':'ir.actions.act_window',
+            'res_model':'car.rental.order',
+            'view_mode':'form',
+            'res_id':self.car_rental_order_id.id
+        }
